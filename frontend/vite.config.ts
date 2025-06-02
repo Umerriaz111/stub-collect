d6dd9c1a9bfc72d69738a46065ec9d80a06c600d
danibuilds <dk301442@gmail.com>
2025-06-02 22:04:43 +0500

    removed minimal theme template and added new simple vite template:"

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,40 +1,7 @@
-import path from 'path';
-import checker from 'vite-plugin-checker';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-// ----------------------------------------------------------------------
-
-const PORT = 8081;
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      typescript: true,
-      eslint: {
-        useFlatConfig: true,
-        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
-        dev: { logLevel: ['error'] },
-      },
-      overlay: {
-        position: 'tl',
-        initialIsOpen: false,
-      },
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.resolve(process.cwd(), 'node_modules/$1'),
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.resolve(process.cwd(), 'src/$1'),
-      },
-    ],
-  },
-  server: { port: PORT, host: true },
-  preview: { port: PORT, host: true },
-});
+  plugins: [vue()],
+})
