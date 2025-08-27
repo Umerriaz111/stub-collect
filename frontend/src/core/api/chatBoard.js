ff2b5104126f1682a925430b06cc9036c8bd5045
danibuilds <dk301442@gmail.com>
2025-08-27 08:10:17 +0500

    resolved the config issue by creating a central api service for all api's by which image issue is resolved too

--- frontend/src/core/api/chatBoard.js
+++ frontend/src/core/api/chatBoard.js
@@ -1,14 +1,7 @@
-import axios from "axios";
-import config from "../services/configService";
-
-const API_BASE_URL = config.VITE_APP_API_BASE_URL;
-
-const api = axios.create({
-  baseURL: API_BASE_URL,
-  withCredentials: true,
-});
+import { getApi } from "./apiService";
 
 export const askChatbot = async (formData) => {
+  const api = await getApi();
   return await api.post("/api/chatbot/chat", formData, {
     headers: {
       "Content-Type": "multipart/form-data", // required for file uploads
