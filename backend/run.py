b85df580af6cd9f44a603fc36a54c0f20645fe7b
UmerRiaz-01 <umer@simming.ai>
2025-09-17 21:50:32 +0500

    change port

--- backend/run.py
+++ backend/run.py
@@ -8,4 +8,4 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5001))  # Render sets PORT, fallback for local
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
